61059087ee8610843916fc919f00555108e495e7
It's meee! Phat Marc <phfatm@gmail.com>
2020-07-10 13:29:02 +0200

    update

--- script/script.js
+++ script/script.js
@@ -1,12 +1,23 @@
-// MENU
+// MENU Dropdowns
 	const drop = document.querySelectorAll(".drpdwn");
 	
 	function toggleActive() {
 		this.classList.toggle("active");
-	  var dropdownContent = this.nextElementSibling;
+	  let dropdownContent = this.nextElementSibling;
 	  dropdownContent.style.display === "block" ? 
 	 	dropdownContent.style.display = "none" : 
 		dropdownContent.style.display = "block";
 	};
 
 	drop.forEach(dropdown => dropdown.addEventListener("click", toggleActive));
+
+	// MENU Hide on scroll
+	let prevScrollpos = window.pageYOffset;
+	
+window.onscroll = function() {
+  let currentScrollPos = window.pageYOffset;
+	prevScrollpos > currentScrollPos ?
+	document.querySelector("nav").style.top = "0" :
+	document.querySelector("nav").style.top = "-68px";
+  prevScrollpos = currentScrollPos;
+} 
